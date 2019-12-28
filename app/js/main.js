dbb683997052eef5f958d427ebda2c9b68d1a653
Zahadym <zahadym@gmail.com>
2019-12-28 11:38:56 +0400

    header slider 28-12 11:38

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,9 @@ $(function(){
 
     $('.header__slider').slick({
         infinite: true,
-        fade: true
+        fade: true,
+        prevArrow: '<img class="slider-arrows sliderarrows__left src="img/icon/arrow-left.svg" alt="arrow">',
+        nextArrow: '<img class="slider-arrows sliderarrows__right src="img/icon/arrow-right.svg" alt="arrow">',
     });
 
 });

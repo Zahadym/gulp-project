6cebfea676ac1503f0ee3b1c4c86173707353810
Zahadym <zahadym@gmail.com>
2019-12-28 12:35:31 +0400

    no work sleder arrows

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ $(function(){
     $('.header__slider').slick({
         infinite: true,
         fade: true,
-        prevArrow: '<img class="slider-arrows sliderarrows__left src="img/icon/arrow-left.svg" alt="arrow">',
-        nextArrow: '<img class="slider-arrows sliderarrows__right src="img/icon/arrow-right.svg" alt="arrow">',
+        prevArrow: '<img class="slider-arrows sliderarrows__left src="img/icon/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows sliderarrows__right src="img/icon/arrow-right.svg" alt="">',
     });
 
 });

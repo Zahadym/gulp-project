5669a9a080ca76142244c7918044881cf5e308b2
Zahadym <zahadym@gmail.com>
2019-12-30 14:35:50 +0400

    header 30-12

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
-$(function(){
+$(function () {
 
     $('.header__slider').slick({
         infinite: true,
         fade: true,
-        prevArrow: '<img class="slider-arrows sliderarrows__left src="img/icon/arrow-left.svg" alt="">',
-        nextArrow: '<img class="slider-arrows sliderarrows__right src="img/icon/arrow-right.svg" alt="">',
+        nextArrow: '<img class="slider-arrows" src="img/icon/arrow-right.svg" alt="arrow">',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/icon/arrow-left.svg" alt="arrow">',
+        asNavFor: '.slider-dots'
+    });
+
+    $('.slider-dots').slick({
+        asNavFor: '.header__slider',
+        slidesToShow: 4,
+        slidesToScroll: 4
     });
 
 });
